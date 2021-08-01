05c1127a0789e4cb90c8778bf7ce061c8def2e7e
Marco Aspeitia <maspeitia.31@gmail.com>
2021-07-31 22:19:21 -0500

    Fixing own-carousel related post and fiixing archive title archive.php

--- archive.php
+++ archive.php
@@ -17,14 +17,7 @@ get_template_part( 'template-parts/header/header', 'search' );
         <h2>
             <?php 
                 echo sprintf( __( '%s Posts de ', 'curso-wordpress' ), $wp_query->found_posts );
-
-                if( get_queried_object(  )->taxonomy == 'post_tag'){
-                    echo 'tag: ' . get_queried_object()->name;
-                }elseif( get_queried_object(  )->taxonomy == 'category' ){
-                    echo 'category: ' . get_queried_object()->name;
-                }else{
-                    echo 'author: ' . get_queried_object()->user_nicename;
-                }
+                the_archive_title();
             ?>
         </h2>
     </div>
